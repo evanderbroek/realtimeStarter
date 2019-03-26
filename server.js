b70cd27ef273b7d18b65845635eaa72d9bad7271
Ethan Vander Broek <evanderbroek@mcad.edu>
2019-03-26 17:08:12 -0500

    prep for heroku

--- server.js
+++ server.js
@@ -96,8 +96,9 @@ io.on('connection', function(socket){
   })
 });
 
+let port = process.env.PORT || 3000 ;
 
 //turn on our server so it can receive requests
-server.listen(3000, function(){
+server.listen(port, function(){
   console.log('app is listening on port 3000');
 });
